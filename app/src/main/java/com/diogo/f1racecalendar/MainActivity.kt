7015f2c307b6dbe04f4ecb74c963eb7fea4c7765
massas24 <diogo.massano.ccmi@gmail.com>
2025-06-19 17:16:29 +0100

    Update MainActivity.kt

--- app/src/main/java/com/diogo/f1racecalendar/MainActivity.kt
+++ app/src/main/java/com/diogo/f1racecalendar/MainActivity.kt
@@ -3,30 +3,18 @@ package com.diogo.f1racecalendar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.diogo.f1racecalendar.ui.ecras.HomeScreen
 import com.diogo.f1racecalendar.ui.theme.F1RaceCalendarTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             F1RaceCalendarTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomeScreen()
             }
         }
     }
 }
+
 
