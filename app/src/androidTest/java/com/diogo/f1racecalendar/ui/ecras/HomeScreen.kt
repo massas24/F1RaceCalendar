968242638e19bae39c5f20da634ff6e502eb0677
massas24 <diogo.massano.ccmi@gmail.com>
2025-06-19 13:39:29 +0100

    Create HomeScreen
    Criei o homescreen
    adicionei os imports

---
+++ app/src/androidTest/java/com/diogo/f1racecalendar/ui/ecras/HomeScreen.kt
@@ -0,0 +1,17 @@
+package com.diogo.f1racecalendar.ui.ecras
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.diogo.f1racecalendar.data.repositorio.RepositorisDeCorridas
+import com.diogo.f1racecalendar.ui.componentes.CartaoCorrida
+
+class HomeScreen {
+}
