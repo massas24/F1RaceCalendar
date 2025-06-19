2845854bc79d3c9522f354aa8617fefe5deb8fcb
massas24 <diogo.massano.ccmi@gmail.com>
2025-06-19 12:27:52 +0100

    Initial
    Initial project setup with Jetpack Compose

---
+++ app/src/main/java/com/diogo/f1racecalendar/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.diogo.f1racecalendar
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.diogo.f1racecalendar.ui.theme.F1RaceCalendarTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            F1RaceCalendarTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    F1RaceCalendarTheme {
+        Greeting("Android")
+    }
+}
