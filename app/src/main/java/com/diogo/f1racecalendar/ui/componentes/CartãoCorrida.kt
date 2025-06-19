8239353e93599206b7d1cece8d93fad9d26e7f3d
massas24 <diogo.massano.ccmi@gmail.com>
2025-06-19 14:25:01 +0100

    Funçao novo(falta cenas)
    Ao clicar no cartao de cada corrida  vai abrir o o mapa url

--- app/src/main/java/com/diogo/f1racecalendar/ui/componentes/CartãoCorrida.kt
+++ app/src/main/java/com/diogo/f1racecalendar/ui/componentes/CartãoCorrida.kt
@@ -1,20 +1,22 @@
 package com.diogo.f1racecalendar.ui.componentes
 
+import android.content.Context
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionContext
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.diogo.f1racecalendar.data.modelo.Corrida
 
 @Composable
-fun CartaoCorrida(corrida: Corrida, aoClicar: () -> Unit) {
+fun CartaoCorrida(corrida: Corrida, context: Context) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .padding(vertical = 8.dp)
-            .clickable { aoClicar() },
+            .clickable { abrirMapa(context, corrida.mapaUrl) },
         elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
     ) {
         Column(modifier = Modifier.padding(16.dp)) {
@@ -26,5 +28,15 @@ fun CartaoCorrida(corrida: Corrida, aoClicar: () -> Unit) {
             Text(text = "Data: ${corrida.data} às ${corrida.hora}")
             Text(text = "Local: ${corrida.localizacao}, ${corrida.pais}")
         }
+    }
+}
+
+private fun abrirMapa(context: Context, url: String) {
+    try {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        context.startActivity(intent)
+    } catch (e: Exception) {
+        e.printStackTrace()
+        //mensagem de erro
     }
 }
