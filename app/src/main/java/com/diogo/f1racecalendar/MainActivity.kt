886504ca554edbd31f3b9a192e8256812b440356
massas24 <diogo.massano.ccmi@gmail.com>
2025-06-20 13:14:55 +0100

    Update Main Activity

--- app/src/main/java/com/diogo/f1racecalendar/MainActivity.kt
+++ app/src/main/java/com/diogo/f1racecalendar/MainActivity.kt
@@ -3,6 +3,10 @@ package com.diogo.f1racecalendar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.remember
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.diogo.f1racecalendar.ui.ecras.HomeScreen
 import com.diogo.f1racecalendar.ui.theme.F1RaceCalendarTheme
 
@@ -11,7 +15,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             F1RaceCalendarTheme {
-                HomeScreen()
+                val navController = rememberNavController()
+
+                NavHost(navController = navController, startDestination = "home"){
+                    composable("home"){
+                        HomeScreen(navController)
+                    }
+
+                    composable(
+                        route = "detalhes/{nome}/{data}/{hora}/{localizacao}/{pais}",
+                        arguments = 
+                    )
+                }
             }
         }
     }
