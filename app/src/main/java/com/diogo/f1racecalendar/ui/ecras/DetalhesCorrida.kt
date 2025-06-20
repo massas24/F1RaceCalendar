3236769445635f10a281b1d78f53dce91bbe23e4
massas24 <diogo.massano.ccmi@gmail.com>
2025-06-20 13:09:01 +0100

    Update DetalhesCorrida

--- app/src/main/java/com/diogo/f1racecalendar/ui/ecras/DetalhesCorrida.kt
+++ app/src/main/java/com/diogo/f1racecalendar/ui/ecras/DetalhesCorrida.kt
@@ -6,16 +6,26 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
+@Composable
 fun DetalhesCorridaScreen(
     nome: String,
     data: String,
     hora: String,
     localizacao: String,
     pais: String
-){
+) {
     Scaffold(
         topBar = {
-            TopAppBar(title = { Text("Detalhes da Corrida")})
+            TopAppBar(title = { Text("Detalhes da Corrida") })
         }
-    )
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .padding(16.dp)
+                .fillMaxSize()
+        ) {
+            
+        }
+    }
 }
