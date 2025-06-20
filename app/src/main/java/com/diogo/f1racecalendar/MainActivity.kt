575a0aa3f3c24ca29339c700cc279c9b0eaba531
massas24 <diogo.massano.ccmi@gmail.com>
2025-06-20 13:18:53 +0100

    Update MainActivity

--- app/src/main/java/com/diogo/f1racecalendar/MainActivity.kt
+++ app/src/main/java/com/diogo/f1racecalendar/MainActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.remember
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.diogo.f1racecalendar.ui.ecras.DetalhesCorridaScreen
 import com.diogo.f1racecalendar.ui.ecras.HomeScreen
 import com.diogo.f1racecalendar.ui.theme.F1RaceCalendarTheme
 
@@ -24,8 +27,18 @@ class MainActivity : ComponentActivity() {
 
                     composable(
                         route = "detalhes/{nome}/{data}/{hora}/{localizacao}/{pais}",
-                        arguments = 
-                    )
+                        arguments = listOf(
+                            navArgument("nome"){ type = NavType.StringType },
+                            navArgument("data"){ type = NavType.StringType },
+                            navArgument("hora"){ type = NavType.StringType },
+                            navArgument("localizacao"){ type = NavType.StringType },
+                            navArgument("pais"){ type = NavType.StringType },
+                        )
+                    ){BackStackEntry ->
+                        DetalhesCorridaScreen(
+                            nome = backStackEntry.arguments?.getString("nome") ?: "",
+                        )
+                    }
                 }
             }
         }
