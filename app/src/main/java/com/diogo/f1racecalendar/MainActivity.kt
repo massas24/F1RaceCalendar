505de338b4ca68022d2d2a4dc31b598c80146896
massas24 <diogo.massano.ccmi@gmail.com>
2025-06-22 16:02:48 +0100

    Ativar tema
    observar o estado do tema e aplicar o tema adequado dinamicamente:

--- app/src/main/java/com/diogo/f1racecalendar/MainActivity.kt
+++ app/src/main/java/com/diogo/f1racecalendar/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.*
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavType
 import androidx.navigation.compose.*
 
@@ -14,11 +17,38 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.diogo.f1racecalendar.data.preferences.ModoEscuro
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val temaCor = ModoEscuro(applicationContext)
+
+        lifecycleScope.launch {
+            val eModoEscuro = temaCor.modoEscuroFlow.first()
+
+            setContent {
+                var modoEscuroAtivado by remember { mutableStateOf(eModoEscuro) }
+
+                F1RaceCalendarTheme (darkTheme = modoEscuroAtivado){
+                    val navController = rememberNavController()
+
+                    NavHost(navController = navController, startDestination = "home"){
+                        composable ("home"){
+                            HomeScreen(
+                                navController = navController,
+                                
+                            )
+                        }
+                    }
+                }
+            }
+        }
+
         setContent {
             F1RaceCalendarTheme {
                 val navController = rememberNavController()
