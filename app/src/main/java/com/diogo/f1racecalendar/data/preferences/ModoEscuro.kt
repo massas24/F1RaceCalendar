4c300e6a9b4fb3215d97248244895e24e10c3e1f
massas24 <diogo.massano.ccmi@gmail.com>
2025-06-22 15:48:18 +0100

    Variaveis modo escuro

--- app/src/main/java/com/diogo/f1racecalendar/data/preferences/ModoEscuro.kt
+++ app/src/main/java/com/diogo/f1racecalendar/data/preferences/ModoEscuro.kt
@@ -1,4 +1,16 @@
 package com.diogo.f1racecalendar.data.preferences
 
-class ModoEscuro {
+import androidx.datastore.preferences.core.booleanPreferencesKey
+
+class ModoEscuro(private val context: Context) {
+    companion object{
+        val DARK_MODE_KEY = booleanPreferencesKey("modo escuro ativado")
+    }
+
+
+    val modoEscuroFlow: Flow<Boolean> = context.dataStore.data
+        .map{preferences ->
+            preferences[DARK_MODE_KEY] ?: false
+
+        }
 }
