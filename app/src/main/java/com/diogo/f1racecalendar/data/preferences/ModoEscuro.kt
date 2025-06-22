21bc63192d2b68852211fe7e4bbc9d47e015664f
massas24 <diogo.massano.ccmi@gmail.com>
2025-06-22 15:51:46 +0100

    Imports
    Adicionei os imports e o setModoEscuro

--- app/src/main/java/com/diogo/f1racecalendar/data/preferences/ModoEscuro.kt
+++ app/src/main/java/com/diogo/f1racecalendar/data/preferences/ModoEscuro.kt
@@ -1,6 +1,13 @@
 package com.diogo.f1racecalendar.data.preferences
 
+import android.content.Context
 import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.Flow
+import androidx.datastore.preferences.core.edit
+
+private val Context.dataStore by preferencesDataStore(name = "definições")
 
 class ModoEscuro(private val context: Context) {
     companion object{
@@ -13,4 +20,10 @@ class ModoEscuro(private val context: Context) {
             preferences[DARK_MODE_KEY] ?: false
 
         }
+
+    suspend fun setModoEscuro(ativado: Boolean) {
+        context.dataStore.edit { preferences ->
+            preferences[DARK_MODE_KEY] = ativado
+        }
+    }
 }
